e2e1d48fe632ff1282a15c96acafc5ba121ded0d
Ronan Noonan <114731040+RazzaNoonan@users.noreply.github.com>
2023-12-06 19:06:05 +0000

    Landing Page complete

--- Library-App/src/app/pages/landing/landing.page.spec.ts
+++ Library-App/src/app/pages/landing/landing.page.spec.ts
@@ -1,15 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { LandingPage } from './landing.page';
+
+
 
 describe('LandingPage', () => {
   let component: LandingPage;
   let fixture: ComponentFixture<LandingPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LandingPage ]
+    })
+    .compileComponents();
+  }));
+  
+  beforeEach(() => {
     fixture = TestBed.createComponent(LandingPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- Library-App/src/app/pages/landing/landing.page.ts
+++ Library-App/src/app/pages/landing/landing.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.page.html',
   styleUrls: ['./landing.page.scss'],
 })
-export class LandingPage implements OnInit {
+export class LandingPage {
+  // Hold messages for feature card read - They are initialized to empty strings.
+  readMessage: string = '';
 
   constructor() { }
-
-  ngOnInit() {
+  // Method to show the read message - Clears after 2 seconds.
+  showReadMessage(): void {
+    this.readMessage = 'Dive into your next favorite book!\n' + 'You can rent books. Up to a wide range';
+    setTimeout(() => this.readMessage = '', 2000);
   }
-
 }
